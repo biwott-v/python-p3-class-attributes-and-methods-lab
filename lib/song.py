9d91326e92c85da1b0cca6188b8ac8b79c5293ef
biwott-v <victor.biwott@student.moringaschool.com>
2025-06-06 13:08:09 +0300

    completes assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment total song count
+        Song.count += 1
+        
+        # Track genres and their count
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+        Song.genre_count[genre] += 1
+        
+        # Track artists and their count
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+        Song.artist_count[artist] += 1
+
+    @classmethod
+    def genres(cls):
+        # Return the keys of genre_count as a list
+        return list(cls.genre_count.keys())
+
+    @classmethod
+    def artists(cls):
+        # Return the keys of artist_count as a list
+        return list(cls.artist_count.keys())
